ef07f605e2d98ccf5620d25270747eeaf94185d0
Blehnig101 <leh24002@byui.edu>
2025-03-31 08:07:06 -0600

    add

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -2,8 +2,26 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        int userchoice = 0;
+
+        Console.WriteLine("To begin you adventure type 1");
+        Console.WriteLine("To end you adventure type 2");
+        userchoice = int.Parse(Console.ReadLine());
+
+        while (userchoice != 2)
+        {
+            if (userchoice == 1)
+            {
+
+            }
+            if (userchoice == 2)
+            {
+                
+            }
+        }
+
     }
 }
